5bea706dc532b11a9fb75a3c9827b9aeb801dc7e
Benjamin Correa <iambenjamin.developer@gmail.com>
2025-05-16 11:35:40 -0300

    Movie Service Template

---
+++ WcfMovieService/IMovieService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace WcfMovieService
+{
+
+    [ServiceContract]
+    public interface IMovieService
+    {
+        [OperationContract]
+        bool AddMovie(Movie movie);
+
+        [OperationContract]
+        List<Movie> GetAllMovies();
+
+        [OperationContract]
+        Movie GetMovieById(int id);
+
+        [OperationContract]
+        bool UpdateMovie(Movie movie);
+
+        [OperationContract]
+        bool DeleteMovie(int id);
+    }
+
+    [DataContract]
+    public class Movie
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Director { get; set; }
+
+        [DataMember]
+        public int Year { get; set; }
+    }
+}
---
+++ WcfMovieService/MovieService.svc.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WcfMovieService
+{
+    public class MovieService : IMovieService
+    {
+        private static List<Movie> _movies = new List<Movie>();
+        private static int _nextId = 1;
+
+        public bool AddMovie(Movie movie)
+        {
+            movie.Id = _nextId++;
+            _movies.Add(movie);
+            return true;
+        }
+
+        public List<Movie> GetAllMovies() => _movies;
+
+        public Movie GetMovieById(int id) => _movies.FirstOrDefault(m => m.Id == id);
+
+        public bool UpdateMovie(Movie updatedMovie)
+        {
+            var movie = _movies.FirstOrDefault(m => m.Id == updatedMovie.Id);
+            if (movie == null) return false;
+
+            movie.Title = updatedMovie.Title;
+            movie.Director = updatedMovie.Director;
+            movie.Year = updatedMovie.Year;
+            return true;
+        }
+
+        public bool DeleteMovie(int id)
+        {
+            var movie = _movies.FirstOrDefault(m => m.Id == id);
+            if (movie == null) return false;
+
+            _movies.Remove(movie);
+            return true;
+        }
+    }
+}
